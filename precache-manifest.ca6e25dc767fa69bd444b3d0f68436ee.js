6a779c3e060ecf075c63592d816187e8cd6831e1
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-06-24 22:29:28 +0300

    Updates

---
+++ precache-manifest.ca6e25dc767fa69bd444b3d0f68436ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd7210b045fb25c2bd84af9e7cd6355",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "ea0481a1047f36cbbb41",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.b724fb76.chunk.css"
+  },
+  {
+    "revision": "bf33857780d8af5c9b2b",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c9804eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c9804eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0481a1047f36cbbb41",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.6926117e.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
